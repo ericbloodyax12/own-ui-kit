f13ead6a64dd49bdb8144177d0c4982bbf369f88
Young Eric <erikbonopart12@gmail.com>
2024-03-26 20:48:45 +0300

    -change structure in BrsArray class -add two new methods of BrsArray: Union and Take

--- src/array/Compact.ts
+++ src/array/Compact.ts
@@ -1,17 +1,34 @@
 /** Creates an array with all falsey values removed.
  *  The values false, null, 0, "", undefined,
  *  and NaN are falsey. **/
-class Class {
-
+export class BrsArray {
   constructor() {
   }
-
-
-}
-export const Compact = (arr: Array<any>): Array<any> => {
+Compact<T> (arr: Array<T>): Array<T> {
   let clearArr
   clearArr = arr.filter((el) => {
-   return  el == true
+    return  el == true
   })
   return clearArr
+}
+Union<T> (arrays: Array<T[]>, unicItems: boolean): T[] {
+  unicItems = true
+  let resArray: T[] = []
+   arrays.forEach((arr) => {
+    arr.forEach((el) => {
+      if (unicItems ? !resArray.includes(el) : true ) {
+        return resArray.push(el)
+      }
+    })
+  })
+  return resArray
+}
+Take<T> (array: T[], n:number): T[] {
+  if (n <= array.length) {
+    return array.slice(0, n);
+  } else {
+    console.error('Ошибка: Значение n превышает длину массива');
+    return array;
+  }
+}
 }
